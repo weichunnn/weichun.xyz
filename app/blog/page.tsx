016fc756bdf9ea1080c91ea7ca48c91ae49ab5c8
Wei Chun <tanweichun@outlook.com>
2023-10-29 02:03:42 +0800

    ending the week with a bang

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,5 @@
 import { allBlogs } from "contentlayer/generated";
-import { compareDesc } from "date-fns";
+import { compareDesc, format } from "date-fns";
 import Link from "next/link";
 import Header from "@/components/Header";
 
@@ -10,12 +10,12 @@ export default function Page() {
 
   return (
     <div>
-      <Header title="Blog Posts" />
+      <Header title="Blog Posts" className="mb-4" />
       {sortedBlogs.map((blog, index) => (
         <Link key={index} href={`/blog/${blog.slug}`}>
           <div className="flex flex-row items-center justify-between">
             <span key={blog._id}>{blog.title}</span>
-            <span>{blog.publishedAt}</span>
+            <span>{format(new Date(blog.publishedAt), "dd-MM-yyyy")}</span>
           </div>
         </Link>
       ))}
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,5 +5,5 @@ export default function Header({
   title: string;
   className?: string;
 }) {
-  return <h1 className={`text-2xl mb-6 font-bold ${className}`}>{title}</h1>;
+  return <h1 className={`text-3xl mb-2 font-bold ${className}`}>{title}</h1>;
 }
