e95015c015cd8c69929c181666a1cbf273cd8eed
Wei Chun <tanweichun@outlook.com>
2024-01-16 03:36:39 +0800

    fix arrow

--- app/now/page.tsx
+++ app/now/page.tsx
@@ -14,14 +14,16 @@ export default function Page() {
       <ul className="list-disc grid gap-2">
         <li>Working</li>
         <li>
-          <CustomLink href="https://lu.ma/socraticakl">Socratica in KL </CustomLink>
+          <CustomLink href="https://lu.ma/socraticakl">
+            Socratica in KL{" "}
+          </CustomLink>
         </li>
         <li>Reading Almanack of Naval Ravikant</li>
         <li>Building product on the side</li>
         <li>Writing/journaling daily</li>
         <li>Applying to graduate school</li>
       </ul>
-      <p className="mt-6">Last updated -> 15th January 2023</p>
+      <p className="mt-6">Last updated -&gt; 15th January 2023</p>
     </>
   );
 }
