cdad2bda2d9b96728e38208feff864b7b167245a
Wei Chun <tanweichun@outlook.com>
2024-01-16 03:26:20 +0800

    update now page

--- app/now/page.tsx
+++ app/now/page.tsx
@@ -10,15 +10,18 @@ export default function Page() {
         <CustomLink href="https://nownownow.com/about">Derek Sivers</CustomLink>
         , I'm writing this page to share what I'm doing now.
       </p>
-
       <h2 className="mt-6 mb-2 font-bold text-lg">On my hands</h2>
       <ul className="list-disc grid gap-2">
-        <li>Work work work</li>
-        <li>Capturing daily learning through writing</li>
-        <li>More hackathons</li>
-        <li>Applying to Grad School</li>
-        <li>Trying to contribute to OSS</li>
+        <li>Working</li>
+        <li>
+          <CustomLink href="https://lu.ma/socraticakl">Socratica in KL </CustomLink>
+        </li>
+        <li>Reading Almanack of Naval Ravikant</li>
+        <li>Building product on the side</li>
+        <li>Writing/journaling daily</li>
+        <li>Applying to graduate school</li>
       </ul>
+      <p className="mt-6">Last updated -> 15th January 2023</p>
     </>
   );
 }
