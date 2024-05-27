3bbcf90171db34272c24809760ff03120ec1a792
weichunnn <tanweichun@outlook.com>
2024-05-27 18:01:58 +0800

    deprecate tags page

--- app/tag/[slug]/page.tsx
+++ app/tag/[slug]/page.tsx
@@ -3,22 +3,23 @@ import BlogList from "@/components/BlogList";
 import Header from "@/components/Header";
 import BlogTags from "@/components/BlogTags";
 
-export async function generateStaticParams() {
-  const uniqueTags = new Set();
-  allBlogs.forEach((blog) => {
-    blog.tags.forEach((tag) => uniqueTags.add(tag));
-  });
-  return Array.from(uniqueTags).map((tag) => ({ slug: tag }));
-}
+// export async function generateStaticParams() {
+//   const uniqueTags = new Set();
+//   allBlogs.forEach((blog) => {
+//     blog.tags.forEach((tag) => uniqueTags.add(tag));
+//   });
+//   return Array.from(uniqueTags).map((tag) => ({ slug: tag }));
+// }
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const { slug } = params;
-  const blogs = allBlogs.filter((blog: Blog) => blog.tags.includes(slug));
+  // const blogs = allBlogs.filter((blog: Blog) => blog.tags.includes(slug));
+  
   return (
     <>
-      <Header title={`Tag: ${slug} (${blogs.length})`} className="mb-4" />
+      {/* <Header title={`Tag: ${slug} (${blogs.length})`} className="mb-4" />
       <BlogTags />
-      <BlogList blogs={blogs} />
+      <BlogList blogs={blogs} /> */}
     </>
   );
 }
